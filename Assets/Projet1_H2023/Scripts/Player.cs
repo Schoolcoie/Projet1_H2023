ed0108a7fabdee8b644e14a88178b996f72fde59
Samuel Cloutier <2130147@collegebart.ca>
2023-01-30 16:30:14 -0500

    Changed movement to axis and started messing with dash and cursor

--- Assets/Projet1_H2023/Scripts/Player.cs
+++ Assets/Projet1_H2023/Scripts/Player.cs
@@ -4,53 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float Speed = 5.0f;
+    private Rigidbody2D PlayerBody;
+    private float PlayerSpeed = 5.0f;
     private Vector2 midPoint;
     private Vector3 midPoint_as_V3;
     private float HalfScreenWidth = Screen.width / 2;
     private float HalfScreenHeight = Screen.height / 2;
+    [SerializeField] private float CameraScrollDistance = 50;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 midPoint = (Input.mousePosition - transform.position);
-        Vector3 midPoint_as_V3 = new Vector3(midPoint.x, midPoint.y, -10);
-        Camera.main.transform.position = transform.position + new Vector3(midPoint_as_V3.x / HalfScreenWidth - 1, midPoint_as_V3.y / HalfScreenHeight - 1, midPoint_as_V3.z);
-
         
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(0, Speed * Time.deltaTime, 0);
-        }
+        float VerticalMovement = Input.GetAxis("Vertical");
+        PlayerBody.velocity = new Vector3(PlayerBody.velocity.x, VerticalMovement * PlayerSpeed);
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -Speed * Time.deltaTime, 0);
-        }
+        float HorizontalMovement = Input.GetAxis("Horizontal");
+        PlayerBody.velocity = new Vector3(HorizontalMovement * PlayerSpeed, PlayerBody.velocity.y);
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(Speed * Time.deltaTime, 0 , 0);
-        }
+        Vector2 midPoint = (Input.mousePosition - transform.position);
+        Vector3 midPoint_as_V3 = new Vector3(midPoint.x, midPoint.y, -10);
+        Camera.main.transform.position = transform.position + new Vector3(midPoint_as_V3.x / HalfScreenWidth * CameraScrollDistance - CameraScrollDistance, midPoint_as_V3.y / HalfScreenHeight * CameraScrollDistance - CameraScrollDistance, midPoint_as_V3.z);
 
+        float DashTimer = 0;
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            transform.position += new Vector3(-Speed * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            Debug.Log(Screen.width);
-            Debug.Log(Screen.height);
+            transform.position = Vector2.Lerp(transform.position, transform.position + Vector3.up, DashTimer);
         }
     }
 }
